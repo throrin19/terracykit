aa0fd890417b8b497017d6d5af91d54e35b5bf81
Benjamin Besse <benjamin.besse02@gmail.com>
2016-01-09 00:21:13 +0100

    @ retur arrière pour le thème : Full Bootstrap avec modification des variables + nouveaux styles et composants

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,35 @@
 
 var gulp        = require('gulp'),
     less        = require('gulp-less'),
+    watch       = require('gulp-watch'),
     watchLess   = require('gulp-watch-less'),
     path        = require('path');
 
-gulp.task('less', function () {
+
+var lessTask =  function () {
     return gulp.src('./less/terracy.less')
-        .pipe(watchLess('less/**.less'))
         .pipe(less({
             filename : 'terracy.less',
             paths: [ path.join(__dirname, 'less') ],
             compress: true
         }))
-        .pipe(gulp.dest('./dist/css/'));
-});
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(gulp.dest('./docs/assets/dist/css/'));
+};
 
-gulp.task('less-doc', function () {
-    return gulp.src('./less/terracy.less')
-        .pipe(watchLess('less/**.less'))
-        .pipe(less({
-            filename : 'terracy.less',
-            paths: [ path.join(__dirname, 'less') ],
-            compress: true
-        }))
-        .pipe(gulp.dest('./docs/dist/css/'));
-});
 
-gulp.task('build', [ 'less', 'less-doc' ], function () {
-    return;
+
+//gulp.task('less-doc', function () {
+//    return gulp.src('./less/terracy.less')
+//        .pipe(watchLess('less/terracy.less'))
+//        .pipe(less({
+//            filename : 'terracy.less',
+//            paths: [ path.join(__dirname, 'less') ],
+//            compress: true
+//        }))
+//        .pipe(gulp.dest('./docs/assets/dist/css/'));
+//});
+
+gulp.task('build', function () {
+    return lessTask();
 });
